c7515bbabf39cd639a5239c7ee4a435a02bad802
shaineafzal <afzals@onid.oregonstate.edu>
2015-02-28 23:13:08 -0800

    Finished Loop Back

---
+++ loopback.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>multtable.php</title>
+</head>
+<body>
+<?php
+
+$array = array();
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+	$array['Type'] = "POST";
+	
+	foreach($_POST as $key => $value){
+		if(is_null($key || $value) == true){
+			$array["parameters"] = "Null";
+		}
+		else
+			$array["parameters"] .= $key. ": ". $value. ", ";
+	}
+}
+else{
+	$array['Type'] = "GET";
+	foreach($_GET as $key => $value){
+		if(is_null($key || $value) == true){
+			$array["parameters"] = "Null";
+		}
+		else
+			$array["parameters"] .= $key. ": ". $value. ", ";
+	}
+}
+
+echo json_encode($array);
+?>
+</body>
+</html>
